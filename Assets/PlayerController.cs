2bc524c36ccc9be958d827feb306acfef2733799
andrew <andrew@warpdevelopment.com>
2019-09-09 18:08:40 +0200

    -

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMoter))]
+public class PlayerController : MonoBehaviour
+{
+	public LayerMask MovementMask;
+
+	Camera camera;
+	PlayerMoter playerMotor;
+
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		camera = Camera.main;
+		playerMotor = GetComponent<PlayerMoter>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			Ray clickRay = camera.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+
+			if (Physics.Raycast(clickRay, out hit, 100, MovementMask))
+			{
+				//Move player
+				playerMotor.MoveToPoint(hit.point);
+
+				//stop focus
+			}
+		}
+	}
+}
---
+++ Assets/PlayerMoter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class PlayerMoter : MonoBehaviour
+{
+
+	NavMeshAgent navMeshagent;
+    // Start is called before the first frame update
+    void Start()
+    {
+		navMeshagent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	public void MoveToPoint(Vector3 destPoint)
+	{
+		navMeshagent.SetDestination(destPoint);
+	}
+}
